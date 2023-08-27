8698b8470091f84cf82d75f256094211ef20f8c7
Adams <adamsh33@gmail.com>
2023-08-27 17:12:04 -0400

    Herencia de plantillas

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,45 +1,15 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """"
-<h1>Mi web personal</h1>
-<ul>
-    <li>
-        <a href="/">Portada</a>
-    </li>
-     <li>
-        <a href="/about">Acerca de</a>
-    </li>
-    <li>
-        <a href="/portfolio">POrtafolio</a>
-    </li>
-    <li>
-        <a href="/contact">Contacto</a>
-    </li>
-</ul>
-"""
-
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-        <h2>Portada</h2>
-        <p>Esto es la portada</p>
-    """)
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(html_base + """
-        <h2>Acerca de</h2>
-        <p>Me llamo Adams y soy programador</p>
-    """)
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-        <h2>Portafolio</h2>
-        <p>Este es mi portafolio</p>
-    """)
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(html_base + """
-        <h2>Contato</h2>
-        <p>Mis datos de contacto</p>
-    """)
+    return render(request, "core/contact.html")
 
