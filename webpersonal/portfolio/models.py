97632690ce1572c81abe2d9a414960c74e0f7e0c
Adams <adamsh33@gmail.com>
2023-11-18 19:17:38 -0300

    Personalizacion de vista admin - Cambio de nombres y titulos y config para cargar imagenes en proyecto y verlas por la vista admin

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -2,8 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Proyect(models.Model):
-    title = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    image = models.ImageField()
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length=200, verbose_name="Titulo")
+    descripcion = models.TextField(verbose_name="Descripcion")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name="Creado en")
+    updated_at = models.DateTimeField(auto_now_add = True, verbose_name="Actualizado en")
+
+    class Meta:
+        verbose_name = "Proyecto" # nombre de la clase
+        verbose_name_plural = "proyectos" # nombre de la clase en plural
+        # campo de organizacion por defecto
+        # El guion -  es para que ordene del mas reciente al mas antiguo
+        ordering = ["-created_at"] 
+
+    # Cambia el nombre del registro en la viosualizacion en pantalla
+    def __str__(self):
+        return self.title
